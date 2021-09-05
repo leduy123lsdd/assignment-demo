1ce79275cfffe474c5b5e10b36e7d324ddbb9f00
duy <duy123galaxy13@gmail.com>
2021-09-05 22:45:04 +0700

    fix workpace

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,4 +1,7 @@
 
 echo "***************************"
 echo "********* Building war ****"
-docker run --rm -v $PWD/hello-world:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
+
+WORKSPACE=/var/lib/jenkins/workspace/main-branch
+
+docker run --rm -v $WORKSPACE/hello-world:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -1,1 +1,3 @@
-docker run --rm -v $PWD/hello-world:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
+
+WORKSPACE=/var/lib/jenkins/workspace/main-branch
+docker run --rm -v $WORKSPACE/hello-world:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
